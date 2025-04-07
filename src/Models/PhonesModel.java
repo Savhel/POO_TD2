b5b684eb50dd3b0b038f53d3173e1904273c2461
wilbed237 <wilsontchoyi@gmail.com>
2025-04-07 23:15:20 +0100

    read phone and equipement

--- src/Models/PhonesModel.java
+++ src/Models/PhonesModel.java
@@ -1,10 +1,19 @@
 package Models;
 
-public class PhonesModel {
+public class PhonesModel extends MaterielsModel {
     private String IMEI;
+
+
+    public PhonesModel(String nom, String marque, String modele, float memoire_ROM, float memoire_RAM, String numero_serie, Integer idPropretaire, String couleur, String etat_Materiel, String IMEI) {
+        super(nom, marque, modele, memoire_ROM, memoire_RAM, numero_serie, idPropretaire, couleur, etat_Materiel);
+        this.IMEI = IMEI;
+    }
 
     public PhonesModel(String IMEI) {
         this.IMEI = IMEI;
+    }
+
+    public PhonesModel() {
     }
 
     public String getIMEI() {
